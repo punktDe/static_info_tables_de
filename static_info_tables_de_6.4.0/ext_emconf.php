7b51879087250fb9780c5bd78cf78e77758c5854
DrFelder <thomaspotzsch@gmail.com>
2016-11-04 12:07:00 +0100

    [TASK] Add repository files via upload
    These files are part of the TYPO3-extension static_info_tables_de

---
+++ static_info_tables_de_6.4.0/ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "static_info_tables_de".
+#
+# Auto generated 13-10-2015 18:45
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static Info Tables (de)',
+	'description' => 'German (de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'shy' => 1,
+	'version' => '6.4.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '6.2.0-7.99.99',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Daniel Minder',
+	'author_email' => 'typo3@minder.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
